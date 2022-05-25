e212700cbb1d7f869092f01c34d287347d9c35d4
amilcar <52044033+Terminal-Redux@users.noreply.github.com>
2022-05-25 06:37:53 -0500

    Fin de seccion 10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
+import { VentasModule } from './ventas/ventas.module';
+
+// Cambiar el locale de la app
+import localeEsMX from '@angular/common/locales/es-MX';
+import localeFrCA from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeEsMX);
+registerLocaleData(localeFrCA);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRouterModule,
+    SharedModule,
+    VentasModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-MX' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -0,0 +1,71 @@
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-no-comunes',
+  templateUrl: './no-comunes.component.html',
+  styles: [
+  ]
+})
+export class NoComunesComponent {
+
+  // i18nSelect
+  nombre: string = 'Fernando';
+  genero: string = 'masculino';
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
+  }
+
+  // i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan', 'Hernando'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemeos un cliente esperando.',
+    'other': 'tenemos # clientes esperando'
+  }
+
+  cambiarPersona() {
+    this.nombre = 'Susanna';
+    this.genero = 'femenino';
+  }
+
+  borrarCliente() {
+    this.clientes.pop();
+  }
+
+  // KeyValue Pipe
+  persona = {
+    nombre: 'Fernando',
+    edad: 35,
+    direccion: 'Ottawa, Canada'
+  }
+
+  // Json Pipe
+  heroes = [
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Robin',
+      vuela: false
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false
+    },
+  ];
+
+  // Async Pipe
+  miObservable = interval(1000);
+
+  valorPromesa = new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+
+  });
+
+}
